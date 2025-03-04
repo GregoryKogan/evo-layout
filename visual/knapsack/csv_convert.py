3a356fd0c4c473441869581f8c44bf8ed5b6fb6a
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-03-05 00:21:29 +0300

    feat: update csv conversion script and add time chart for knapsack

--- visual/knapsack/csv_convert.py
+++ visual/knapsack/csv_convert.py
@@ -2,15 +2,26 @@ import json
 import os
 
 script_path = os.path.dirname(os.path.realpath(__file__))
-logfile = os.path.join(script_path, "knapsack.jsonl")
+logfile = os.path.join(script_path, "Knapsack.jsonl")
 
-csv_lines = ["generation,fitness\n"]
+csv_lines = ["elapsed,generation,fitness\n"]
 
 with open(logfile) as f:
     f.readline()
-    for line in f:
-        data = json.loads(line)
-        csv_lines.append(f'{data["generation"]},{data["solution"]["fitness"]}\n')
+    lines = f.readlines()
+
+max_fitness = json.loads(lines[-1])["solution"]["fitness"]
+for line in lines:
+    data = json.loads(line)
+    if "took" in data:
+        max_fitness = data["solution"]["fitness"]
+        print(
+            f"Algorithmic solution took: {int(data['took']) / 1e9}s, reaching fitness: {max_fitness}"
+        )
+        continue
+    csv_lines.append(
+        f'{int(data["elapsed"]) / 1e9},{data["generation"]},{data["solution"]["fitness"] / max_fitness * 100}\n'
+    )
 
 output = os.path.join(script_path, "knapsack.csv")
 with open(output, "w") as f:
