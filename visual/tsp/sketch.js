d576d6e34d1ec363db7f00df83129b32450e6841
Gregory Koganovsky <grisha.koganovskiy@gmail.com>
2025-03-04 22:59:04 +0300

    feat: add TSP visualization with HTML and p5.js integration

---
+++ visual/tsp/sketch.js
@@ -0,0 +1,81 @@
+let problem;
+let algoSolution;
+let solutions;
+let loaded = false;
+let done = false;
+
+let generation = 0;
+
+function setup() {
+  createCanvas(min(windowWidth, windowHeight), min(windowWidth, windowHeight));
+  textSize(32);
+  fetch("TSP.jsonl").then((response) => {
+    response.text().then((data) => {
+      const parsed = JSON.parse(
+        "[" +
+          data
+            .split("\n")
+            .filter((line) => line.length > 2)
+            .join(",") +
+          "]"
+      );
+      problem = parsed[0];
+      algoSolution = parsed[1];
+      solutions = parsed.slice(2);
+      loaded = true;
+    });
+  });
+  frameRate(5);
+  saveGif("planar-graph.gif", 30);
+}
+
+function draw() {
+  if (!loaded || done) return;
+
+  if (generation >= solutions.length) {
+    done = true;
+    console.log("Animation frames", frameCount);
+    return;
+  }
+
+  const solution = solutions[generation].solution;
+  generation++;
+
+  background(18);
+
+  document.querySelector("#length").innerHTML = `Length: ${round(
+    1 / solution.fitness
+  )}`;
+
+  for (let city of problem.cities) {
+    fill(255);
+    const v = toScreenCoord(city.lat, city.lon);
+    circle(v.x, v.y, 10);
+  }
+
+  let curCity = problem.cities[0];
+  for (let cityInd of solution.order) {
+    const city = problem.cities[cityInd];
+    stroke(255);
+    strokeWeight(2);
+    line(
+      toScreenCoord(curCity.lat, curCity.lon).x,
+      toScreenCoord(curCity.lat, curCity.lon).y,
+      toScreenCoord(city.lat, city.lon).x,
+      toScreenCoord(city.lat, city.lon).y
+    );
+    curCity = city;
+  }
+  line(
+    toScreenCoord(curCity.lat, curCity.lon).x,
+    toScreenCoord(curCity.lat, curCity.lon).y,
+    toScreenCoord(problem.cities[0].lat, problem.cities[0].lon).x,
+    toScreenCoord(problem.cities[0].lat, problem.cities[0].lon).y
+  );
+}
+
+function toScreenCoord(x, y) {
+  x /= 100;
+  y /= 100;
+  return createVector(50 + x * (width - 100), 50 + y * (height - 100));
+}
